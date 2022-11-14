b2c6d887cbedcbc34eb6c3404e9c9db059412d55
air <kobychev00@bk.ru>
2022-11-14 17:53:20 +0300

    conditionals p1 homework

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        int age = 18;
+        //задание 1
+        System.out.println("Задание 1");
+        int age = 8;
         if (age >= 18) {
             System.out.println("Поздравляем! Ты можешь водить");
         } else {
             System.out.println("Придется подождать до совершеннолетия");
         }
-
-        int ageHuman = 25;
+        //задание 2
+        System.out.println("Задание 2");
+        int ageHuman = 8;
         if (ageHuman >= 7 && ageHuman < 18) {
             System.out.println("Завтра в школу");
         }
@@ -18,10 +21,11 @@ public class Main {
         if (ageHuman >= 24) {
             System.out.println("Завтра на работу");
         }
-
-        int seatsOccupied = 100;
-        var allSpace = 120;
-        var allseats = 60;
+        //задание 3
+        System.out.println("Задание 3");
+        int seatsOccupied = 102;
+        int allSpace = 120;
+        int allseats = 60;
         if (seatsOccupied < allseats) {
             System.out.println("Есть сидячие места");
         }
@@ -31,8 +35,9 @@ public class Main {
         if (seatsOccupied >= allSpace) {
             System.out.println("Мест нет");
         }
-
-        int age2 = 27;
+        //задание 3
+        System.out.println("Задание 3");
+        int age2 = 8;
         if (age2> 2 && age2 < 6) {
             System.out.println("Если возраст" + age2 + ", то нужно ходить в детский сад");
         }
@@ -45,8 +50,9 @@ public class Main {
         if (age2 > 24) {
             System.out.println("Если возраст " + age2 + " , то Твое место на работе");
         }
-
-        int age3 = 27;
+        //задание 4
+        System.out.println("Задание 4");
+        int age3 = 8;
         if (age3< 5) {
             System.out.println("Тебе запрещено кататься на аттракционе");
         }
@@ -56,7 +62,8 @@ public class Main {
         if (age3 > 14) {
             System.out.println("Тебе можно кататься на аттракционе без сопровождения");
         }
-
+        //задание 5
+        System.out.println("Задание 5");
         int one = 1;
         int two = 2;
         int free = 3;
